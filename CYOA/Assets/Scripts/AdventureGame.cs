b21d4da54f6372afb558c47a44b98a880c85d001
Chad Smith <chadsmith4@my.unt.edu>
2018-06-17 03:21:20 -0500

    -- added a test intro scene and initial design

---
+++ CYOA/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] private Text _textComponent; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        _textComponent.text = "Our amazing story!";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
