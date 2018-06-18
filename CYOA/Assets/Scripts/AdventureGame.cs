20e1942e9cf669ef68ae864158fefdf98bb4db4d
Chad Smith <chadsmith4@my.unt.edu>
2018-06-18 01:44:54 -0500

    -- Added states and text mesh pro. Game is completed.

--- CYOA/Assets/Scripts/AdventureGame.cs
+++ CYOA/Assets/Scripts/AdventureGame.cs
@@ -1,19 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour
 {
-    [SerializeField] private Text _textComponent; 
+    [SerializeField] private Text _textComponent;
+    [SerializeField] private State _startingState;
+
+    private State _state;
 
 	// Use this for initialization
 	void Start ()
     {
-        _textComponent.text = "Our amazing story!";
+        _state = _startingState;
+        _textComponent.text = _state.StoryText;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        ManageState();
 	}
+
+    private void ManageState()
+    {
+        var nextStates = _state.NextStates;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _state = nextStates[2];
+        }
+
+        _textComponent.text = _state.StoryText;
+    }
 }
---
+++ CYOA/Assets/Scripts/State.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)]
+    [SerializeField]
+    private string _storyText;
+    [SerializeField]
+    private State[] _nextStates;
+
+    /// <summary>
+    /// The story text to print out for this current state.
+    /// </summary>
+    public string StoryText
+    {
+        get
+        {
+            return _storyText;
+        }
+    }
+
+    /// <summary>
+    /// Gets the next states that you can go to from this state.
+    /// </summary>
+    public State[] NextStates
+    {
+        get
+        {
+            return _nextStates;
+        }
+    }
+}
